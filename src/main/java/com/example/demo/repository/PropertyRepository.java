f116f88e821f458fd711369942a8cde0f8644ecd
unknown <juan.fernandez-g@labinfo.is.escuelaing.edu.co>
2025-03-05 17:02:20 -0500

    ya corre bien

--- src/main/java/com/example/demo/repository/PropertyRepository.java
+++ src/main/java/com/example/demo/repository/PropertyRepository.java
@@ -2,10 +2,9 @@ package com.example.demo.repository;
 
 import com.example.demo.model.Property;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
-
-public interface PropertyRepository extends CrudRepository<Property, Long> {
+@Repository
+public interface PropertyRepository extends JpaRepository<Property, Long> {
 
 }
